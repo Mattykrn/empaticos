e0a56b1d9b7a70d60ad5360acafa35c8cb6d4bc9
Mattykrn <matii.toorres.06@gmail.com>
2026-03-26 19:19:58 -0300

    Subida final de Firebase

--- src/pages/js/firebase-config.js
+++ src/pages/js/firebase-config.js
@@ -2,14 +2,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
-// TODO: Matii, REEMPLAZAR ESTE OBJETO CON EL QUE TE DA FIREBASE AL CREAR EL PROYECTO
+// Tu configuración web de Firebase
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyA_OiKkVu-2_VWuUxEgGGIlDf8U4un24o4",
+  authDomain: "empaticosbd.firebaseapp.com",
+  projectId: "empaticosbd",
+  storageBucket: "empaticosbd.firebasestorage.app",
+  messagingSenderId: "723912898582",
+  appId: "1:723912898582:web:84438d3f899e58070d9d37"
 };
 
 // Inicializar Firebase
